3a954053f9afa68ace9c50822382e6f91a406948
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-05-22 15:24:55 -0500

    :hammer: Control de errores en los métodos web.

--- src/main/java/com/ws/masterhelpdesk/controller/UserController.java
+++ src/main/java/com/ws/masterhelpdesk/controller/UserController.java
@@ -1,21 +1,28 @@
 package com.ws.masterhelpdesk.controller;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ws.masterhelpdesk.dto.UserDto;
+import com.ws.masterhelpdesk.exception.ApiError;
 import com.ws.masterhelpdesk.model.service.IUserService;
 
 import lombok.AllArgsConstructor;
@@ -52,5 +59,32 @@ public class UserController {
 	public ResponseEntity<HttpStatus> updateUser(@RequestBody @Valid UserDto userDto) {
 		iUserService.updateUser(userDto);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		return ex.getBindingResult().getAllErrors().stream()
+				.map(err -> new ApiError(err.getCodes(), err.getDefaultMessage())).collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public List<ApiError> handleValidationExceptions(ConstraintViolationException ex) {
+		return ex.getConstraintViolations().stream()
+				.map(err -> new ApiError(err.getPropertyPath().toString(), err.getMessage()))
+				.collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(RuntimeException.class)
+	public List<ApiError> handleNotFoundExceptions(RuntimeException ex) {
+		return Collections.singletonList(new ApiError("User Not Found", ex.getMessage()));
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	public List<ApiError> handleOtherException(Exception ex) {
+		return Collections.singletonList(new ApiError(ex.getClass().getCanonicalName(), ex.getMessage()));
 	}
 }
---
+++ src/main/java/com/ws/masterhelpdesk/exception/ApiError.java
@@ -0,0 +1,15 @@
+package com.ws.masterhelpdesk.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+	private String[] codes;
+	private String message;
+
+	public ApiError(String code, String message) {
+		this(new String[] { code }, message);
+	}
+}
