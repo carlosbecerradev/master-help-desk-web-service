475c422d97944296bb6d05e7a002e0d39588aa2e
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-02 13:18:25 -0500

    :hammer: Mantenimiento de Clientes: Validación de datos agregado.

--- src/main/java/com/ws/masterhelpdesk/dto/CustomerDto.java
+++ src/main/java/com/ws/masterhelpdesk/dto/CustomerDto.java
@@ -2,6 +2,11 @@ package com.ws.masterhelpdesk.dto;
 
 import java.time.Instant;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,17 +17,29 @@ import lombok.Setter;
 public class CustomerDto {
 
 	private Long id;
-	
+
+	@NotBlank
+	@Size(min = 2, max = 50)
 	private String name;
-	
+
+	@NotBlank
+	@Size(min = 2, max = 50)
 	private String surname;
 
+	@NotBlank
+	@Email
+	@Size(max = 100)
 	private String email;
 
+	@NotBlank
+	@Size(max = 15)
 	private String cellphone;
 
+	@NotBlank
+	@Size(max = 20)
 	private String gender;
 
+	@NotNull
 	private Boolean enabled;
 
 	private Instant createdAt;
--- src/main/java/com/ws/masterhelpdesk/dto/insert/CustomerInsert.java
+++ src/main/java/com/ws/masterhelpdesk/dto/insert/CustomerInsert.java
@@ -1,5 +1,10 @@
 package com.ws.masterhelpdesk.dto.insert;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,19 +14,35 @@ import lombok.Setter;
 @Builder
 public class CustomerInsert {
 
+	@NotBlank
+	@Size(min = 2, max = 50)
 	private String name;
 
+	@NotBlank
+	@Size(min = 2, max = 50)
 	private String surname;
 
+	@NotBlank
+	@Email
+	@Size(max = 100)
 	private String email;
 
+	@NotBlank
+	@Size(max = 15)
 	private String cellphone;
 
+	@NotBlank
+	@Size(max = 20)
 	private String gender;
 
+	@NotNull
 	private Boolean enabled;
 
+	@NotBlank
+	@Size(min = 6, max = 20)
 	private String username;
 
+	@NotBlank
+	@Size(min = 6, max = 60)
 	private String password;
 }
