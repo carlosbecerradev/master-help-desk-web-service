df43a0d1dcd27051666590213779b12fec62e75c
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-02 16:57:44 -0500

    :hammer: Mantenimiento de Empleados: Control de errores en métodos web agregado.

--- src/main/java/com/ws/masterhelpdesk/controller/EmployeeController.java
+++ src/main/java/com/ws/masterhelpdesk/controller/EmployeeController.java
@@ -1,24 +1,31 @@
 package com.ws.masterhelpdesk.controller;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ws.masterhelpdesk.dto.EmployeeDto;
 import com.ws.masterhelpdesk.dto.insert.EmployeeInsert;
+import com.ws.masterhelpdesk.exception.ApiError;
 import com.ws.masterhelpdesk.model.service.IEmployeeService;
 
 import lombok.AllArgsConstructor;
@@ -58,4 +65,32 @@ public class EmployeeController {
 		iEmployeeService.deleteEmployee(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		return ex.getBindingResult().getAllErrors().stream()
+				.map(err -> new ApiError(err.getCodes(), err.getDefaultMessage())).collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public List<ApiError> handleValidationExceptions(ConstraintViolationException ex) {
+		return ex.getConstraintViolations().stream()
+				.map(err -> new ApiError(err.getPropertyPath().toString(), err.getMessage()))
+				.collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(RuntimeException.class)
+	public List<ApiError> handleNotFoundExceptions(RuntimeException ex) {
+		return Collections.singletonList(new ApiError("Employee Not Found", ex.getMessage()));
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	public List<ApiError> handleOtherException(Exception ex) {
+		return Collections.singletonList(new ApiError(ex.getClass().getCanonicalName(), ex.getMessage()));
+	}
+
 }
