034424ac2bc2e5216181fcb6f1ebaffe217d6d57
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-06-08 16:07:06 -0500

    :hammer: TicketController: Control de errores en métodos web.

--- src/main/java/com/ws/masterhelpdesk/controller/TicketController.java
+++ src/main/java/com/ws/masterhelpdesk/controller/TicketController.java
@@ -1,16 +1,25 @@
 package com.ws.masterhelpdesk.controller;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ws.masterhelpdesk.dto.insert.TicketInsert;
+import com.ws.masterhelpdesk.exception.ApiError;
 import com.ws.masterhelpdesk.model.service.ITicketService;
 
 import lombok.AllArgsConstructor;
@@ -28,4 +37,32 @@ public class TicketController {
 		iTicketService.insert(ticketInsert);
 		return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
 	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		return ex.getBindingResult().getAllErrors().stream()
+				.map(err -> new ApiError(err.getCodes(), err.getDefaultMessage())).collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ConstraintViolationException.class)
+	public List<ApiError> handleValidationExceptions(ConstraintViolationException ex) {
+		return ex.getConstraintViolations().stream()
+				.map(err -> new ApiError(err.getPropertyPath().toString(), err.getMessage()))
+				.collect(Collectors.toList());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(RuntimeException.class)
+	public List<ApiError> handleNotFoundExceptions(RuntimeException ex) {
+		return Collections.singletonList(new ApiError("Ticket Not Found", ex.getMessage()));
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Exception.class)
+	public List<ApiError> handleOtherException(Exception ex) {
+		return Collections.singletonList(new ApiError(ex.getClass().getCanonicalName(), ex.getMessage()));
+	}
+
 }
